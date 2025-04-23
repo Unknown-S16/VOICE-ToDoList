6d3f8db9f9b0723d46658b3622b66fb51e224857
Unknown-S16 <sharath20038@gmail.com>
2025-04-24 00:31:18 +0530

    Initial commit

--- backend/server.js
+++ backend/server.js
@@ -2,16 +2,15 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const taskRoutes = require('./routes/tasks');
+const dotenv =require('dotenv');
 
+dotenv.config();
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use('/api/tasks', taskRoutes);
 
-mongoose.connect('mongodb+srv://lama:lama@mern.xmwoh.mongodb.net/?retryWrites=true&w=majority&appName=mern', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-}).then(() => {
+mongoose.connect(process.env.MongoDB).then(() => {
   console.log('MongoDB connected');
-  app.listen(5000, () => console.log('Server started on port 5000'));
+  app.listen( process.env.PORT , () => console.log('Server started on port 5000'));
 }).catch(err => console.log(err));
