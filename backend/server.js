f6c637e68968a2c6d757c9a45b844db8d35ad42b
Unknown-S16 <sharath20038@gmail.com>
2025-04-24 01:01:10 +0530

    CORS update

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,13 @@ const dotenv =require('dotenv');
 
 dotenv.config();
 const app = express();
-app.use(cors());
+
+
+app.use(cors({
+  origin: ["http://localhost:5173", "https://voice-to-do-list.vercel.app"],
+  credentials: true
+}));
+
 app.use(express.json());
 app.use('/api/tasks', taskRoutes);
 
