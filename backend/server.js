e64cd1518edbe1998655601bcdce4da980a70fe8
Unknown-S16 <sharath20038@gmail.com>
2025-06-09 12:18:59 +0530

    design update

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-  origin: ["https://voice-to-do-list.vercel.app"],
+  origin: "https://voice-to-do-list.vercel.app",
   credentials: true
 }));
 
