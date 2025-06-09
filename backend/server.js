60f81ee9e47de37e506b7b7cbf8ed1cb58fbb442
Unknown-S16 <sharath20038@gmail.com>
2025-06-09 12:13:48 +0530

    design update

--- backend/server.js
+++ backend/server.js
@@ -2,21 +2,25 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const taskRoutes = require('./routes/tasks');
-const dotenv =require('dotenv');
+const dotenv = require('dotenv');
 
 dotenv.config();
 const app = express();
 
-
 app.use(cors({
-  origin: ["http://localhost:5173", "https://voice-to-do-list.vercel.app"],
+  origin: ["https://voice-to-do-list.vercel.app"],
   credentials: true
 }));
 
 app.use(express.json());
 app.use('/api/tasks', taskRoutes);
 
+// Add this root route:
+app.get('/', (req, res) => {
+  res.send('API is live and running!');
+});
+
 mongoose.connect(process.env.MongoDB).then(() => {
   console.log('MongoDB connected');
-  app.listen( process.env.PORT , () => console.log('Server started on port 5000'));
+  app.listen(process.env.PORT, () => console.log('Server started on port', process.env.PORT));
 }).catch(err => console.log(err));
